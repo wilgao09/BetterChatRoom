4a1a5263ffb96c4270316363cc94d9bb46f0fcfd
wilgao09 <william.gao09@gmail.com>
2020-06-20 15:00:28 -0400

    fixed frontend-testing not getting css

--- frontend-testing.js
+++ frontend-testing.js
@@ -10,8 +10,10 @@
 var http = require("http");
 var pug = require('pug');
 var fs = require('fs');
+var sass = require('node-sass');
+var ncp = require('ncp').ncp;
 
-var fileName = "dashboard";
+var fileName = "index";
 
 var compiledPugFunc = pug.compileFile("views/"+fileName + ".pug");
 
@@ -21,4 +23,42 @@ var HTMLstr = compiledPugFunc(testTexts);
 
 fs.writeFileSync("frontend-testing/"+fileName+".html", HTMLstr);
 
-console.log("Done!");
+console.log("Compiled HTML");
+
+/**
+ * Just render all of the sass and stuff
+ */
+
+ fs.readdir("public/stylesheets", function(err, files) {
+    console.log(files);
+    for (var n = 0 ;n != files.length; n++) {
+        // check if sass or scss file
+        var ext = files[n].substring(files[n].indexOf("."));
+        if (ext == ".sass" || ext == ".scss") {
+            console.log("processing " + files[n]);
+            var result = sass.renderSync({file:"public/stylesheets/"+files[n]});
+            var fileName = files[n].substring(0,files[n].indexOf("."));
+            fs.writeFile("frontend-testing/stylesheets/" + fileName + ".css", result.css.toString(), function(err) {
+                console.log("Finished rendering");
+            
+            });
+            // sass.render({file:"public/stylesheets/"+files[n]}, function(err, result) {
+            //     console.log(files[n]);
+            //     console.log(n);
+            //     console.log("^^files");
+            //     var fileName = files[n].substring(0,files[n].indexOf("."));
+            //     fs.writeFile("frontend-testing/stylesheets/" + fileName + ".css", result.css.toString(), function(err) {
+            //         console.log("error in rednering");
+            //         console.log(err);
+            //     });
+            // })
+        }
+    }
+ })
+
+ console.log("Compiled Sass!");
+
+ncp('public/javascript','frontend-testing/javascript', function(err) {
+    console.log("Copied JS!");
+});
+
---
+++ frontend-testing/javascript/forms.js
@@ -0,0 +1,35 @@
+/**
+ * Custom forms so I can actually hear back from server
+ */
+
+ /**
+  * Submit data to a server, POST by default
+  * @param {*} strId the id for the form (or the element containing the labels)
+  */
+function submitForm(strId, action, callback, method="POST") {
+    var url = "http://" + window.location.hostname + ":6689" + action;
+    var labels = document.getElementById(strId).getElementsByTagName("label");
+    var inputs = [];
+    for (var n = 0 ; n != labels.length; n++) {
+        var inps = labels[n].getElementsByTagName("input");
+        if (inps.length > 0) inputs.push(inps[0]);
+    }
+    if (inputs.length > 0) url += "?";
+    console.log(inputs);
+    for (var ind = 0; ind != inputs.length; ind++) {
+        if (ind != 0) url += "&";
+        url += inputs[ind].name + "=" + inputs[ind].value.trim(); 
+    }
+    console.log(url);
+    // nBody = {}
+    // for (var n = 0 ;n != inputs.length; n++) {
+    //     nBody[inputs[n].name] = inputs[n].value;
+    // }
+    // console.log(nBody);
+    var req = new XMLHttpRequest();
+    req.open(method,url,true);
+    req.onload = callback;
+    // console.log(JSON.stringify(req));
+    req.send();
+}
+
